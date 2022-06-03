c09025ff5f61bc88a98ab5c7fa4e34378b195c47
Chia-An Lee <sz110010@gmail.com>
2022-06-03 15:19:07 +0800

    Time: 6 ms (94.85%), Space: 6.2 MB (96.32%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,30 +1,12 @@
 class Solution {
+    int mem[310][310] = {};
 public:
     int minKnightMoves(int x, int y) {
-        int tx = abs(x) + 2;
-        int ty = abs(y) + 2;
-        // cout << tx << " " << ty << endl;
-        int step[310][310] = {};
-        pair<int,int> dir[] = {{2,1}, {1,2}, {2,-1}, {1,-2}, {-2,-1}, {-1,-2}, {-2,1}, {-1,2}};
-        queue<pair<int,pair<int,int>>> q;
-        q.push({0,{2,2}});
-        while (!q.empty()) {
-            int cx = q.front().second.first;
-            int cy = q.front().second.second;
-            int lev = -q.front().first;
-            q.pop();
-            for (auto & d: dir) {
-                int nx = cx + d.first;
-                int ny = cy + d.second;
-                // cout << nx << " " << ny << endl;
-                if (nx == 2 && ny == 2) continue;
-                if (nx >= 0 && ny >= 0 && nx < 310 && ny < 310 && step[nx][ny] == 0) {
-                    if (nx == tx && ny == ty) return lev+1;
-                    step[nx][ny] = lev + 1;
-                    q.push({-lev-1, {nx, ny}});
-                }
-            }
-        }
-        return step[tx][ty];
+        x = abs(x); y = abs(y);
+        if (mem[x][y] != 0) return mem[x][y];
+        if (x + y == 0) return 0;
+        if (x + y == 2) return 2;
+        mem[x][y] = min(minKnightMoves(x-2, y-1), minKnightMoves(x-1, y-2)) + 1;
+        return mem[x][y];
     }
 };
