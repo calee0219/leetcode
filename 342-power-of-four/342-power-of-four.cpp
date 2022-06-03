153175360b9bbb91f0a7bae9f797eba84d4f9401
Chia-An Lee <sz110010@gmail.com>
2022-06-03 21:46:16 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (71.16%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return (n > 0) && !(__builtin_ctz(n) & 1) && !(n & (n-1));
+        return (n > 0) && !(__builtin_ctz(n) & 1) &&  (__builtin_popcount(n) == 1);
     }
 };
