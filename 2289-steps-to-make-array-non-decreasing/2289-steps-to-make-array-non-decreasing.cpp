908600cd94bcae6ec93a5a22b91d6cb3f30ad6d6
Chia-An Lee <sz110010@gmail.com>
2022-06-05 19:56:04 +0800

    Time: 256 ms (51.07%), Space: 74 MB (86.37%) - LeetHub

---
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int totalSteps(vector<int>& nums) {
+        stack<int> sk;
+        int prev_max = 0;
+        int ans = 0;
+        for (int idx = nums.size()-1; idx >= 0; --idx) {
+            int popCnt = 0;
+            prev_max = max(prev_max, (int)sk.size());
+            while (!sk.empty() && (sk.top() < nums[idx])) {
+                popCnt += 1;
+                sk.pop();
+            }
+            // ans = max(ans, popCnt);
+            if (prev_max > sk.size())
+                ans = max(ans, (int)(prev_max - sk.size()));
+            sk.push(nums[idx]);
+            
+            // cout << popCnt << " " << sk.top() << endl;
+        }
+        return ans;
+    }
+};
