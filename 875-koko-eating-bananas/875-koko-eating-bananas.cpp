7828cb6d331c58c43b58f5d832178f1255f9cc53
Chia-An Lee <sz110010@gmail.com>
2022-06-08 16:43:56 +0800

    Time: 75 ms (35.37%), Space: 18.9 MB (67.02%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,21 @@
+int n;
+
+class Solution {
+    int check(vector<int> & piles) {
+        auto rd_func = [](int a, int b) { return a + b / n + (b % n != 0); };
+        return accumulate(piles.begin(), piles.end(), 0, rd_func);
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1, r = accumulate(piles.begin(), piles.end(), 0, [](int a, int b) {return max(a, b);}) + 1;
+        while (l < r) {
+            n = l + (r-l) / 2;
+            if (check(piles) > h) {
+                l = n+1;
+            } else {
+                r = n;
+            }
+        }
+        return l;
+    }
+};
