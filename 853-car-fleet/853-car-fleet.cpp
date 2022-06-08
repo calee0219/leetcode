5e9afb6e305ec821fa0555ad0507c142bb54c00a
Chia-An Lee <sz110010@gmail.com>
2022-06-08 15:56:19 +0800

    Time: 676 ms (5.13%), Space: 78.5 MB (70.77%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<pair<int,int>> arr; // position, speed
+        for (int idx = 0; idx < n; ++idx) {
+            arr.push_back({position[idx], speed[idx]});
+        }
+        sort(arr.begin(), arr.end());
+        stack<pair<double,double>> sk;
+        sk.push(arr[0]);
+        for (int idx = 1; idx < n; ++idx) {
+            while(!sk.empty() && (sk.top().second > arr[idx].second) &&
+                  (sk.top().first-arr[idx].first)/(arr[idx].second-sk.top().second)*arr[idx].second+arr[idx].first <= target) {
+                sk.pop();
+            }
+            sk.push(arr[idx]);
+            cout << sk.size() << endl;
+        }
+        return sk.size();
+    }
+};
