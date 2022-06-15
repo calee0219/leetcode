ea8bb392792a207dffbaa9a4e78675dcd0fef028
Chia-An Lee <sz110010@gmail.com>
2022-06-15 16:22:57 +0800

    Time: 96 ms (76.55%), Space: 70.9 MB (8.74%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        if (accumulate(gas.begin(), gas.end(), 0) < accumulate(cost.begin(), cost.end(), 0))
-            return -1;
+        // if (accumulate(gas.begin(), gas.end(), 0) < accumulate(cost.begin(), cost.end(), 0))
+        //     return -1;
         int n = gas.size();
         vector<int> diff (n, 0);
+        int total = 0;
         for (int idx = 0; idx < n; ++idx) {
             diff[idx] = gas[idx] - cost[idx];
+            total += diff[idx];
             // cout << diff[idx] << " ";
         }
+        if (total < 0) return -1;
         int ans = 0;
         int sum = 0;
         for (int idx = 0; idx < n; ++idx) {
