ee82f65f3475f3f665beb61140e7a5e4f8c525e2
Chia-An Lee <sz110010@gmail.com>
2022-06-15 10:17:55 +0800

    Time: 1628 ms (5.62%), Space: 702.6 MB (5.62%) - LeetHub

---
+++ 527-word-abbreviation/527-word-abbreviation.cpp
@@ -0,0 +1,56 @@
+struct TrieNode {
+    string str;
+    int index;
+    int count;
+    unordered_map<char,TrieNode*> next;
+    TrieNode(): str(""),index(-1),count(0) {}
+    TrieNode(string s): str(s),index(-1),count(0) {}
+};
+
+class Solution {
+    void dfs(TrieNode * node, string pass, vector<string> & ret) {
+        if (node->next.size() == 0) {
+            // cout << pass << " " << node->str << endl;
+            if (node->str.size() - pass.size() > 2) {
+                // cout << pass << (node->str.size() - pass.size() - 1) << node->str[node->str.size()-1] << endl;
+                ret[node->index] = pass + to_string(node->str.size() - pass.size() - 1) + node->str[node->str.size()-1];
+            } else {
+                // cout << node->str << endl;
+                ret[node->index] = node->str;
+            }
+            return;
+        }
+        if (pass == "" && node->count == 1)
+            pass = node->str;
+        for (auto & it: node->next) {
+            dfs(it.second, pass, ret);
+        }
+    }
+public:
+    vector<string> wordsAbbreviation(vector<string>& words) {
+        int words_size = words.size();
+        unordered_map<int, TrieNode *> trieForest[256];
+        // build trie
+        for (int idx = 0; idx < words_size; ++idx) {
+            if (trieForest[words[idx][words[idx].size()-1]].find(words[idx].size()) == trieForest[words[idx][words[idx].size()-1]].end()) {
+                trieForest[words[idx][words[idx].size()-1]].insert({words[idx].size(), new TrieNode});
+            }
+            TrieNode * curr = trieForest[words[idx][words[idx].size()-1]][words[idx].size()];
+            string tmp = "";
+            for (char ch: words[idx]) {
+                tmp += ch;
+                if (curr->next.find(ch) == curr->next.end())
+                    curr->next.insert({ch, new TrieNode(tmp)});
+                curr = curr->next[ch];
+                curr->count += 1;
+            }
+            curr->index = idx;
+        }
+        // iterate trie
+        vector<string> ret (words_size, "");
+        for (char ch = 'a'; ch <= 'z'; ++ch)
+            for (auto & head: trieForest[ch])
+                dfs(head.second, "", ret);
+        return ret;
+    }
+};
