cb32413002bf287814289fec0e52165794b7e1c3
Chia-An Lee <sz110010@gmail.com>
2022-06-15 16:24:16 +0800

    Time: 150 ms (18.11%), Space: 69.3 MB (99.15%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -4,23 +4,19 @@ public:
         // if (accumulate(gas.begin(), gas.end(), 0) < accumulate(cost.begin(), cost.end(), 0))
         //     return -1;
         int n = gas.size();
-        vector<int> diff (n, 0);
         int total = 0;
-        for (int idx = 0; idx < n; ++idx) {
-            diff[idx] = gas[idx] - cost[idx];
-            total += diff[idx];
-            // cout << diff[idx] << " ";
-        }
-        if (total < 0) return -1;
         int ans = 0;
         int sum = 0;
         for (int idx = 0; idx < n; ++idx) {
-            sum += diff[idx];
+            sum += gas[idx] - cost[idx];
             if (sum < 0) {
                 ans = idx + 1;
                 sum = 0;
             }
+            total += gas[idx] - cost[idx];
+            // cout << diff[idx] << " ";
         }
+        if (total < 0) return -1;
         return ans;
     }
 };
