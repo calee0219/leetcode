4f45fd5c01b008e6af6d802eff61962b7e79604b
Chia-An Lee <sz110010@gmail.com>
2022-06-15 13:48:13 +0800

    Time: 14 ms (27.44%), Space: 13.8 MB (27.48%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        if (cost.size() <= 1) return 0;
+        int cosz = cost.size();
+        vector<int> dp (cost.size(), 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int idx = 2; idx < cosz; ++idx) {
+            dp[idx] = min(dp[idx-1], dp[idx-2]) + cost[idx];
+        }
+        return min(dp[cosz-1], dp[cosz-2]);
+    }
+};
