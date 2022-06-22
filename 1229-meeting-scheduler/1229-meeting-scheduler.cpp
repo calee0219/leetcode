90f5024aeff6a63543e15165e28df8d209736a90
Chia-An Lee <sz110010@gmail.com>
2022-06-22 20:35:33 +0800

    Time: 226 ms (63.64%), Space: 40.2 MB (60.55%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.cpp
+++ 1229-meeting-scheduler/1229-meeting-scheduler.cpp
@@ -3,16 +3,19 @@ public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
         priority_queue<pair<int,int>> pq;
         for (auto & s: slots1) {
-            pq.push({-s[0], -s[1]});
+            if (s[0] + duration <= s[1])
+                pq.push({-s[0], -s[1]});
         }
         for (auto & s: slots2) {
-            pq.push({-s[0], -s[1]});
+            if (s[0] + duration <= s[1])
+                pq.push({-s[0], -s[1]});
         }
+        if (pq.empty()) return {};
         pair<int,int> p1, p2;
         p1 = pq.top(); pq.pop();
         while(!pq.empty()) {
             p2 = pq.top(); pq.pop();
-            if (p2.first - duration >= p1.second && p2.first - duration >= p2.second)
+            if (p2.first - duration >= p1.second)
                 return {-p2.first, -p2.first + duration};
             if (p1.second > p2.second)
                 p1 = p2;
