9a6e02622729b8c601a19e71d0c5eeb48fac8c4e
Chia-An Lee <sz110010@gmail.com>
2022-06-22 21:05:18 +0800

    Time: 7 ms (88.42%), Space: 5.9 MB (86.07%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int> um ({{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}});
+        // unordered_map<char,int> um ({{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}});
+        int um[256]; um['I'] = 1; um['V'] = 5; um['X'] = 10; um['L'] = 50; um['C'] = 100; um['D'] = 500; um['M'] = 1000;
         int ans = 0;
         for (int idx = 0; idx < s.size(); ++idx) {
             if (idx+1 < s.size() && um[s[idx]] < um[s[idx+1]])
