341e582f2786dc147a0399477c3887118ff93fc3
Chia-An Lee <sz110010@gmail.com>
2022-06-28 22:44:16 +0800

    Time: 38 ms (61.85%), Space: 27.1 MB (10.75%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        bool check = false;
+        for (int idx = 0; idx < nums.size() - 1; ++idx) {
+            if (nums[idx] > nums[idx+1]) {
+                if (check) return false;
+                if (idx == 0 || nums[idx+1] >= nums[idx-1]) check = true;
+                else {
+                    nums[idx+1] = nums[idx];
+                }
+                check = true;
+            }
+        }
+        return true;
+    }
+};
