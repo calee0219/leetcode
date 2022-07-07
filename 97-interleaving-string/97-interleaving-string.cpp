f908e6b0623e620067469f77f2e442953aec9928
Chia-An Lee <sz110010@gmail.com>
2022-07-08 00:03:45 +0800

    Time: 9 ms (48.94%), Space: 6.6 MB (67.56%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
         if (s3.size() != s1.size() + s2.size()) return false;
-        vector<vector<bool>> dp (s1.size()+1, vector<bool>(s2.size()+1, false));
+        int sz1 = s1.size(), sz2 = s2.size();
+        vector<vector<bool>> dp (sz1+1, vector<bool>(sz2+1, false));
         dp[0][0] = true;
-        for (int p1 = 0; p1 < s1.size(); ++p1) {
+        for (int p1 = 0; p1 < sz1; ++p1) {
             dp[p1+1][0] = dp[p1][0] && s1[p1] == s3[p1];
         }
-        for (int p2 = 0; p2 < s2.size(); ++p2) {
+        for (int p2 = 0; p2 < sz2; ++p2) {
             dp[0][p2+1] = dp[0][p2] && s2[p2] == s3[p2];
         }
-        for (int p1 = 0; p1 < s1.size(); ++p1) {
-            for (int p2 = 0; p2 < s2.size(); ++p2) {
+        for (int p1 = 0; p1 < sz1; ++p1) {
+            for (int p2 = 0; p2 < sz2; ++p2) {
                 dp[p1+1][p2+1] = (dp[p1][p2+1] && s1[p1] == s3[p1+p2+1])
                               || (dp[p1+1][p2] && s2[p2] == s3[p1+p2+1]);
             }
@@ -22,6 +23,6 @@ public:
         //     }
         //     cout << endl;
         // }
-        return dp[s1.size()][s2.size()];
+        return dp[sz1][sz2];
     }
 };
