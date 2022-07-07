dde6516a6960177d61d96d7b126f2dd8c41723fc
Chia-An Lee <sz110010@gmail.com>
2022-07-08 00:02:33 +0800

    Time: 4 ms (75.77%), Space: 6.7 MB (67.56%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,19 +1,27 @@
 class Solution {
-    vector<vector<bool>> mem;
-    bool test(string & t, int pt, string & s1, int p1, string & s2, int p2) {
-        if (pt == t.size()) return true;
-        if (p1 < s1.size() && p2 < s2.size() && mem[p1][p2]) return false;
-        if (p1 < s1.size() && s1[p1] == t[pt] && test(t, pt+1, s1, p1+1, s2, p2))
-            return true;
-        if (p2 < s2.size() && s2[p2] == t[pt] && test(t, pt+1, s1, p1, s2, p2+1))
-            return true;
-        if(p1 < s1.size() && p2 < s2.size()) mem[p1][p2] = true;
-        return false;
-    }
 public:
     bool isInterleave(string s1, string s2, string s3) {
         if (s3.size() != s1.size() + s2.size()) return false;
-        mem.resize(s1.size(), vector<bool>(s2.size(), false));
-        return test(s3, 0, s1, 0, s2, 0);
+        vector<vector<bool>> dp (s1.size()+1, vector<bool>(s2.size()+1, false));
+        dp[0][0] = true;
+        for (int p1 = 0; p1 < s1.size(); ++p1) {
+            dp[p1+1][0] = dp[p1][0] && s1[p1] == s3[p1];
+        }
+        for (int p2 = 0; p2 < s2.size(); ++p2) {
+            dp[0][p2+1] = dp[0][p2] && s2[p2] == s3[p2];
+        }
+        for (int p1 = 0; p1 < s1.size(); ++p1) {
+            for (int p2 = 0; p2 < s2.size(); ++p2) {
+                dp[p1+1][p2+1] = (dp[p1][p2+1] && s1[p1] == s3[p1+p2+1])
+                              || (dp[p1+1][p2] && s2[p2] == s3[p1+p2+1]);
+            }
+        }
+        // for (int p1 = 0; p1 <= s1.size(); ++p1) {
+        //     for (int p2 = 0; p2 <= s2.size(); ++p2) {
+        //         cout << dp[p1][p2] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return dp[s1.size()][s2.size()];
     }
 };
