ffe99f37d34adfd999ba77034b41ea0aa13a47cb
Chia-An Lee <sz110010@gmail.com>
2022-08-05 16:51:30 +0800

    Time: 244 ms (5.03%), Space: 6.7 MB (65.95%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    vector<int> mem;
+    int rec(vector<int>& nums, int target) {
+        if (target == 0) return 1;
+        if (target < 0) return 0;
+        if (mem[target] != -1) return mem[target];
+        int ans = 0;
+        for (int n: nums) {
+            ans += combinationSum4(nums, target-n);
+        }
+        // cout << target << " ";
+        mem[target] = ans;
+        return ans;
+    }
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        mem.resize(1001, -1);
+        sort(nums.begin(), nums.end(), [](int a, int b) { return a < b; });
+        return rec(nums, target);
+    }
+};
